ffa4e4888c0988c4580de0520e4fb933daa56b81
karen327 <2857809611@qq.com>
2022-11-30 19:31:38 +0800

    second

--- chap6/Hw6_4.java
+++ chap6/Hw6_4.java
@@ -7,22 +7,35 @@ import java.util.Scanner;
 public class Hw6_4 {
     public static void main(String[] args) {
         int n1, n2;
-        List<Integer> list = new ArrayList<>();
+        List<Integer> resx = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         n1 = Integer.parseInt(sc.next());
         n2 = n1;
 
-//        while(n1 > 2){
-//            int res1 = minPrime(n1);
-//        }
+        while(n1 > 2){
+            List<Integer> res1 = minPrime(n1);
+            for(int i : res1) {
+                if(n1 % i == 0) {
+                    resx.add(i);
+                    n1 /= i;
+                    break;
+                }
+            }
+        }
+        System.out.print(n2 + "=");
+        int len = resx.size();
+        for(int i = 0; i < len - 1; i ++) {
+            System.out.print(resx.get(i) + "*");
+        }
+        System.out.print(resx.get(len - 1));
 
     }
 
-    public static int minPrime(int n){
-        int[] res = null;
+    public static List<Integer> minPrime(int n){
+        List<Integer> res = new ArrayList<>();
         if(n <= 2){
             System.out.println("not a prime");
-            return -1;
+            return null;
         } else {
             for(int i = 2; i < n + 1; i ++){
                 boolean flag = true;
@@ -33,10 +46,10 @@ public class Hw6_4 {
                     }
                 }
                 if(flag){
-
+                    res.add(i);
                 }
             }
         }
-        return 0;
+        return res;
     }
 }
